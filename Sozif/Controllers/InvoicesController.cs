44bb38c390c9d09f82882d0410fd41e8767984f9
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2019-10-31 18:55:38 +0100

    new invoices

--- Sozif/Controllers/InvoicesController.cs
+++ Sozif/Controllers/InvoicesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Sozif;
 using Sozif.Attributes;
+using Sozif.Models;
 
 namespace Sozif.Controllers
 {
@@ -24,7 +26,9 @@ namespace Sozif.Controllers
         // GET: Invoices
         public async Task<IActionResult> Index()
         {
-            var sozifContext = _context.Invoices.Include(i => i.InvoicePositions);
+            var sozifContext = _context.Invoices
+                .Include(i => i.InvoicePositions)
+                .OrderByDescending(i => i.InvoiceId);
             return View(await sozifContext.ToListAsync());
         }
 
@@ -47,36 +51,181 @@ namespace Sozif.Controllers
             return View(invoices);
         }
 
-        // GET: Invoices/Create
-        public IActionResult Create()
+        // GET: Invoices/ChooseCustomer
+        public async Task<IActionResult> ChooseCustomer()
         {
             if (HttpContext.Session.GetString("EditInvoices") == "false")
             {
                 return RedirectToAction("Index", "Home");
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerName");
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Firstname");
-            return View();
+
+            var customers = await _context.Customers.Include(c => c.Addresses).OrderBy(c => c.CustomerName).ToListAsync();
+            return View(customers);
         }
 
-        // POST: Invoices/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("InvoiceId,InvoiceNumber,CustomerName,CustomerNip,CustomerAddress,CustomerPostalCode,CustomerCity,InvoiceDate,DaysToPay,UserName,CustomerId,UserId")] Invoices invoices)
+        // GET: Invoices/ChooseOrders/5
+        public async Task<IActionResult> ChooseOrders(int? id)
         {
             if (HttpContext.Session.GetString("EditInvoices") == "false")
             {
                 return RedirectToAction("Index", "Home");
             }
-            if (ModelState.IsValid)
+            if (id == null)
             {
-                _context.Add(invoices);
+                return NotFound();
+            }
+            var customer = await _context.Customers.Include(c => c.Addresses).FirstOrDefaultAsync(c => c.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            var ordersToChooseFrom = await _context.Orders
+                .Where(o => !o.OrderNumber.Contains("R"))
+                .Include(o => o.Address)
+                .Include(o => o.User)
+                .Include(o => o.OrderPositions)
+                    .ThenInclude(op => op.Product)
+                    .ThenInclude(p => p.TaxRate)
+                .Where(o => o.CustomerId == id && o.InvoiceId == null)
+                .ToListAsync();
+            InvoiceDTO invoice = new InvoiceDTO();
+            invoice.CustomerName = customer.CustomerName;
+            invoice.CustomerNip = customer.NipString;
+            invoice.CustomerAddress = customer.Addresses.First(a => a.IsMainAddress).FullAddress;
+            ordersToChooseFrom.ForEach(o => invoice.Orders.Add(new KeyValuePair<int, bool>(o.OrderId, false)));
+            ViewBag.Orders = ordersToChooseFrom;
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/ChooseOrders/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChooseOrders(int? id, [Bind("Orders")] InvoiceDTO invoice)
+        {
+            if (HttpContext.Session.GetString("EditInvoices") == "false")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.Include(c => c.Addresses).FirstOrDefaultAsync(c => c.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            string userIdString = HttpContext.Session.GetString("UserId");
+            if (userIdString == null)
+            {
+                return NotFound();
+            }
+            int userId = int.Parse(userIdString);
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
+            var ordersToInvoiceIds = new List<int>();
+            invoice.Orders.ForEach(o => { if (o.Value) ordersToInvoiceIds.Add(o.Key); });
+            if (ordersToInvoiceIds.Count == 0)
+            {
+                ViewBag.ErrorMessage = "Musisz wybrać przynajmniej jedno zamówienie, do którego chcesz wystawić fakturę!";
+            }
+            if (ViewBag.ErrorMessage == null)
+            {
+                var lastInvoiceInDB = await _context.Invoices
+                    .OrderByDescending(o => o.InvoiceId)
+                    .FirstOrDefaultAsync();
+                string month = DateTime.Now.ToString("MM", DateTimeFormatInfo.InvariantInfo);
+                string year = DateTime.Now.ToString("yy", DateTimeFormatInfo.InvariantInfo);
+                string newInvoiceNumber;
+                if (lastInvoiceInDB == null)
+                {
+                    newInvoiceNumber = "F/001/" + month + "/" + year;
+                }
+                else
+                {
+                    string[] lastInvoiceNumberSplit = lastInvoiceInDB.InvoiceNumber.Split('/');
+                    int numberInt;
+                    DateTime lastInvoiceDate = lastInvoiceInDB.InvoiceDate;
+                    DateTime todayDate = DateTime.Now;
+                    if (lastInvoiceDate.Month == todayDate.Month && lastInvoiceDate.Year == todayDate.Year)
+                    {
+                        numberInt = int.Parse(lastInvoiceNumberSplit[1]);
+                    }
+                    else
+                    {
+                        numberInt = 1;
+                    }
+                    string numberString = "" + (numberInt + 1);
+                    while (numberString.Length < 3)
+                    {
+                        numberString = "0" + numberString;
+                    }
+                    newInvoiceNumber = "F/" + numberString + "/" + month + "/" + year;
+                }
+                var newInvoice = new Invoices();
+                newInvoice.InvoiceNumber = newInvoiceNumber;
+                newInvoice.CustomerName = customer.CustomerName;
+                newInvoice.CustomerNip = customer.Nip;
+                var customerMainAddress = customer.Addresses.First(a => a.IsMainAddress);
+                newInvoice.CustomerAddress = customerMainAddress.Street;
+                newInvoice.CustomerPostalCode = customerMainAddress.PostalCode;
+                newInvoice.CustomerCity = customerMainAddress.City;
+                newInvoice.InvoiceDate = DateTime.Now;
+                newInvoice.DaysToPay = invoice.DaysToPay;
+                newInvoice.UserName = user.Firstname + " " + user.Lastname;
+                newInvoice.CustomerId = customer.CustomerId;
+                newInvoice.UserId = userId;
+                _context.Add(newInvoice);
+                await _context.SaveChangesAsync();
+
+                var ordersToInvoice = await _context.Orders
+                    .Where(o => ordersToInvoiceIds.Contains(o.OrderId))
+                    .Include(o => o.OrderPositions)
+                    .ThenInclude(op => op.Product)
+                    .ThenInclude(p => p.TaxRate)
+                    .ToListAsync();
+                var newInvoicePositions = new List<InvoicePositions>();
+                ordersToInvoice.ForEach(o =>
+                {
+                    foreach (OrderPositions op in o.OrderPositions)
+                    {
+                        var invoicePosition = new InvoicePositions();
+                        invoicePosition.ProductName = op.Product.ProductName;
+                        invoicePosition.ProductCount = op.Count;
+                        invoicePosition.ProductNetPrice = op.Product.BaseNetPrice;
+                        invoicePosition.ProductTaxRate = op.Product.TaxRate.Rate;
+                        invoicePosition.Discount = op.Discount;
+                        invoicePosition.InvoiceId = newInvoice.InvoiceId;
+                        newInvoicePositions.Add(invoicePosition);
+                    }
+                });
+                _context.AddRange(newInvoicePositions);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerName", invoices.CustomerId);
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Firstname", invoices.UserId);
-            return View(invoices);
+            var ordersToChooseFrom = await _context.Orders
+                .Where(o => !o.OrderNumber.Contains("R"))
+                .Include(o => o.Address)
+                .Include(o => o.User)
+                .Include(o => o.OrderPositions)
+                .ThenInclude(op => op.Product)
+                .ThenInclude(p => p.TaxRate)
+                .Where(o => o.CustomerId == id && o.InvoiceId == null)
+                .ToListAsync();
+            ViewBag.Orders = ordersToChooseFrom;
+            invoice.Orders.Clear();
+            ordersToChooseFrom.ForEach(o => invoice.Orders.Add(new KeyValuePair<int, bool>(o.OrderId, false)));
+            invoice.CustomerName = customer.CustomerName;
+            invoice.CustomerNip = customer.NipString;
+            invoice.CustomerAddress = customer.Addresses.First(a => a.IsMainAddress).FullAddress;
+
+            return View(invoice);
         }
 
         private bool InvoicesExists(int id)
---
+++ Sozif/Models/InvoiceDTO.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Sozif.Models
+{
+    public class InvoiceDTO
+    {
+
+        public InvoiceDTO()
+        {
+            Orders = new List<KeyValuePair<int, bool>>();
+        }
+
+        public string CustomerName { get; set; }
+        public string CustomerNip { get; set; }
+        public string CustomerAddress { get; set; }
+        public int DaysToPay { get; set; }
+
+        [BindProperty]
+        public List<KeyValuePair<int, bool>> Orders { get; set; }
+
+    }
+}
