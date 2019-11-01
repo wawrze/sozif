2ae341d8455171240517f82c9f03572581b9783d
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2019-11-01 09:06:25 +0100

    password change page

--- Sozif/Controllers/LoginController.cs
+++ Sozif/Controllers/LoginController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Sozif.Models;
 using Sozif.Utils;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,20 +20,19 @@ namespace Sozif.Controllers
             _context = context;
         }
 
-        // GET: /<controller>/
+        // GET: Login
         public IActionResult Index()
         {
-            ViewBag.HideMenu = true;
             return View();
         }
 
-        // POST: Login/Login
+        // POST: Login
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Login([Bind("Username,Password")] LoginDTO loginDTO)
+        public async Task<IActionResult> Index([Bind("Username,Password")] LoginDTO loginDTO)
         {
             String message = "";
-            Users user = _context.Users.SingleOrDefault(user => user.Username == loginDTO.Username);
+            Users user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginDTO.Username);
 
             if (user == null)
             {
@@ -59,7 +60,94 @@ namespace Sozif.Controllers
                     return RedirectToAction("Index", "Orders");
                 }
             }
-            ViewBag.LoginMessage = message;
+            ViewBag.ErrorMessage = message;
+
+            return View();
+        }
+
+        // GET: Login/PasswordChange
+        public IActionResult PasswordChange(int? id)
+        {
+            string userIdString = HttpContext.Session.GetString("UserId");
+            if(userIdString == null)
+            {
+                return NotFound();
+            }
+            if (id == 0)
+            {
+                ViewBag.SuccessMessage = "Hasło zostało zmienione.";
+            }
+            ViewBag.OldPassword = "";
+            ViewBag.NewPassword = "";
+            ViewBag.NewPasswordConfirmation = "";
+            return View();
+        }
+
+        // POST: Login/PasswordChange
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PasswordChange([Bind("OldPassword,NewPassword,NewPasswordConfirmation")] PasswordChangeDTO passwordChange)
+        {
+            string userIdString = HttpContext.Session.GetString("UserId");
+            if (userIdString == null)
+            {
+                return NotFound();
+            }
+            int userId = int.Parse(userIdString);
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            if (passwordChange.OldPassword == null || passwordChange.NewPassword == null || passwordChange.NewPasswordConfirmation == null)
+            {
+                ViewBag.ErrorMessage = "Musisz wypełnić wszystkie pola!";
+                ViewBag.OldPassword = "";
+                ViewBag.NewPassword = "";
+                ViewBag.NewPasswordConfirmation = "";
+                return View();
+            }
+            ViewBag.OldPassword = passwordChange.OldPassword;
+            ViewBag.NewPassword = passwordChange.NewPassword;
+            ViewBag.NewPasswordConfirmation = passwordChange.NewPasswordConfirmation;
+            string passwordInDB = user.Password;
+            if (!PasswordHelper.VerifyHashedPassword(passwordInDB, passwordChange.OldPassword))
+            {
+                ViewBag.ErrorMessage = "Podałeś błędne hasło!";
+                ViewBag.OldPassword = "";
+            }
+            if (!passwordChange.NewPassword.Any(c => char.IsDigit(c)))
+            {
+                ViewBag.ErrorMessage = "Hasło musi zawierać przynajmniej jedną cyfrę!";
+            }
+            if (passwordChange.NewPassword.IndexOfAny("!@#$%^&*?_~-£().,".ToCharArray()) == -1)
+            {
+                ViewBag.ErrorMessage = "Nowe hasło musi zawierać przynajmniej jeden znak specjalny!";
+            }
+            if (!passwordChange.NewPassword.Any(c => char.IsUpper(c)))
+            {
+                ViewBag.ErrorMessage = "Nowe hasło musi zawierać małe i duże litery!";
+            }
+            if (!passwordChange.NewPassword.Any(c => char.IsLower(c)))
+            {
+                ViewBag.ErrorMessage = "Nowe hasło musi zawierać małe i duże litery!";
+            }
+            if (passwordChange.NewPassword.Length < 8)
+            {
+                ViewBag.ErrorMessage = "Hasło musi zawierać przynajmniej 8 znaków!";
+            }
+            if (passwordChange.NewPassword != passwordChange.NewPasswordConfirmation)
+            {
+                ViewBag.ErrorMessage = "Podane hasła różnią się!";
+                ViewBag.NewPasswordConfirmation = "";
+            }
+            if (ViewBag.ErrorMessage == null)
+            {
+                user.Password = PasswordHelper.HashPassword(passwordChange.NewPassword);
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("PasswordChange", new { id = 0 });
+            }
 
             return View();
         }
---
+++ Sozif/Models/PasswordChangeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Sozif.Models
+{
+    public class PasswordChangeDTO
+    {
+
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirmation { get; set; }
+
+    }
+}
